5bcb20cc87dc95375b0af7fc3c51942cdb13b9b6
AnoHobby <96468681+AnoHobby@users.noreply.github.com>
2025-02-21 13:42:37 +0900

    Add files via upload

---
+++ algorithm/math/factorial.cpp
@@ -0,0 +1,18 @@
+﻿import <print>;
+import <iostream>;
+auto for_factorial(auto n) {
+	decltype(n) result = 1;
+	for (; 1 < n; --n) {
+		result *= n;
+	}
+	return result;
+}
+int recursive_factorial(auto n) {
+	if (1 < n)return recursive_factorial(n-1)*n;
+	return 1;
+}
+auto main() -> int {
+	const auto n = *std::istream_iterator<int>(std::cin);
+	std::print("for:{},recursive:{}",for_factorial(n),recursive_factorial(n));
+	return 0;
+}
